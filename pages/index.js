a64ded2b4d4cb10cc0c78b38fe0091e0076649bd
Igor Santos <131270133+IgoorSantoss@users.noreply.github.com>
2024-11-20 21:58:03 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Sarah, eu te amo demais! Se você me ama, me manda uma foto dando uma risadinha no whastapp :D</h1>
+  return (
+    <h1>
+      Sarah, eu te amo demais! Se você me ama, me manda uma foto dando uma
+      risadinha no whastapp :D
+    </h1>
+  );
 }
 
 export default Home;
